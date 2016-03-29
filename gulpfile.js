76ba22fe737004190b40af01b98febc33ee185d5
Hobo Bob <jesse@jesseweed.com>
2016-03-28 20:44:22 -0700

    convert svn to icon font & adjust settings view

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require('gulp'),
+    iconfont = require('gulp-iconfont'),
+    iconfontCss = require('gulp-iconfont-css'),
+    fontName = 'seti';
+
+gulp.task('font', function(){
+  gulp.src(['./icons/*.svg'])
+    .pipe(iconfontCss({
+      fontName: fontName,
+      path: './styles/_fonts/_template.less',
+      targetPath: '../seti.less/',
+      fontPath: './styles/_fonts/seti/'
+    }))
+    .pipe(iconfont({
+      normalize: true,
+      fontName: fontName,
+      formats: ['ttf', 'eot', 'woff', 'woff2', 'svg']
+     }))
+    .pipe(gulp.dest('./styles/_fonts/seti/'));
+});
